23e1e4b946c363335b5bd349009cde93eaa6b8a5
Alexandr <digilevich.alexandr@gmail.com>
2015-07-30 17:20:30 +0300

    1

---
+++ Day7Task4/BinerySearch.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7Task4
+{
+    static class BinerySearch<T> where T : IComparable
+    {
+        public static bool IfExist(T[] a,T k)
+        {
+            Array.Sort<T>(a);
+            int first = 0;
+            int last = a.Length - 1;
+            int cmp;
+            if (a.Length == 0)
+                return false;
+            else
+                if ((cmp=a[0].CompareTo(k)) > 0)
+                    return false;
+                else
+                    if ((cmp=a[0].CompareTo(k)) > 0)
+                        return false;
+
+            while(first < last)
+            {
+                int mid=first + (last - first) / 2;
+
+        if ((cmp=k.CompareTo(a[mid]))<=0)
+            last = mid;
+        else
+            first = mid + 1;
+            }
+
+        if ((cmp=a[last].CompareTo(k))==0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Day7Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new[] { "petya", "galya", "katya", "yura" };
+            Console.WriteLine(BinerySearch<String>.IfExist(array,"petya"));
+            Console.WriteLine(BinerySearch<String>.IfExist(array, "nepetya"));
+            Console.Read();
+        }
+    }
+}
